28a37796e20771cae3e474243ed4c037b4d36505
Charles Naut <cpn585@gmail.com>
2020-05-06 21:56:10 -0400

    Make request to twilio to send SMS with briefing time

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/dghubble/go-twitter/twitter"
@@ -24,9 +27,42 @@ func main() {
 
 	for _, tweet := range tweets {
 		if strings.Contains(tweet.Text, "briefing") && strings.Contains(tweet.Text, "ET") {
-			fmt.Println(tweet.Text)
 			timeEndIndex := strings.Index(tweet.Text, "ET")
-			fmt.Println(tweet.Text[timeEndIndex-8 : timeEndIndex+2])
+			briefingTime := tweet.Text[timeEndIndex-8 : timeEndIndex+2]
+			fmt.Println(tweet.Text)
+			fmt.Println(briefingTime)
+
+			accountSid := ""
+			authToken := ""
+			urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+
+			// Pack up the data for our message
+			msgData := url.Values{}
+			msgData.Set("To", "+17183442807")
+			msgData.Set("From", "+12513068087")
+			msgData.Set("Body", "Watch Cuomo daily briefing at "+briefingTime)
+			msgDataReader := *strings.NewReader(msgData.Encode())
+
+			// Create HTTP request client
+			client := &http.Client{}
+			req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+			req.SetBasicAuth(accountSid, authToken)
+			req.Header.Add("Accept", "application/json")
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+			// Make HTTP POST request and return message SID
+			resp, _ := client.Do(req)
+			if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+				var data map[string]interface{}
+				decoder := json.NewDecoder(resp.Body)
+				err := decoder.Decode(&data)
+				if err == nil {
+					fmt.Println(data["sid"])
+				}
+			} else {
+				fmt.Println(resp.Status)
+			}
+
 			return
 		}
 	}
